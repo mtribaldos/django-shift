bf0eb36c5f96503e4136a028a043dec62c54ae08
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2016-09-26 19:51:58 +0200

    Implemented a form view to provide the shift swap action

--- shifts/views.py
+++ shifts/views.py
@@ -1,11 +1,15 @@
 # -*- encoding: utf-8 -*-
 
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Shifts
+from .forms import ShiftChangeForm
 from .utils import current_year_week
 
 
-def onduty_today(request):
+def onduty_current_week(request):
     week = current_year_week()
     return HttpResponse(Shifts.onduty_person_name(week))
 
@@ -13,6 +17,20 @@ def onduty_next_week(request):
     week = current_year_week() + 1
     return HttpResponse(Shifts.onduty_person_name(week))
 
+@login_required(login_url='/login/')
 def reset(request):
     Shifts.assign()
     return HttpResponse("La asignación de guardias ha sido reseteada")
+
+@login_required(login_url='/login/')
+def shift_change(request):
+    if request.method == 'POST':
+        form = ShiftChangeForm(request.POST)
+        if form.is_valid():
+            Shifts.swap(form.cleaned_data.get('first_date'),
+                        form.cleaned_data.get('second_date'))
+            return HttpResponseRedirect('/')
+    else:
+        form = ShiftChangeForm()
+
+    return render(request, 'shift_change_form.html', {'form': form})
