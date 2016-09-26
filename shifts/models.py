b76ff4ab712d402f07cdf2e4c6315556a07a0641
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2016-09-26 19:46:31 +0200

    Defined _swap_ class method in Staff model to perform a shift swap

--- shifts/models.py
+++ shifts/models.py
@@ -6,6 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User, Group
 from django.db import models
 from django.conf import settings
+from .utils import year_week
 
 
 class StaffManager(models.Manager):
@@ -36,6 +37,14 @@ class Shifts(models.Model):
     def onduty_person_name(cls, week):
        onduty = cls.objects.get(week=week)
        return onduty.user.first_name
+
+    @classmethod
+    def swap(cls, first_date, second_date):
+       onduty_first_date = cls.objects.get(week=year_week(first_date))
+       onduty_second_date = cls.objects.get(week=year_week(second_date))
+       onduty_first_date.user, onduty_second_date.user = onduty_second_date.user, onduty_first_date.user
+       onduty_first_date.save()
+       onduty_second_date.save()
 
     @classmethod
     def assign(cls):
