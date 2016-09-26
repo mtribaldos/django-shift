e9f458ed2816a0f8cb278fa79857db0257392eec
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2016-09-26 19:42:49 +0200

    Created a more generic _year_week_ util function as well

--- shifts/utils.py
+++ shifts/utils.py
@@ -1,6 +1,9 @@
 import datetime
 
+def year_week(date):
+    return date.isocalendar()[1]
+
 def current_year_week():
-    week = datetime.datetime.now().date().isocalendar()[1]
-    return week
+    today = datetime.datetime.now().date()
+    return year_week(today)
 
