85494bc3466c395f16850076eb07a85a146523db
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2016-09-26 19:44:53 +0200

    Redefined all the supported URLs from the scratch

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,18 @@
 """django_shifts URL Configuration
 """
+from django.contrib.auth.views import login
 from django.conf.urls import url
+from django.views.generic import RedirectView
 from django.contrib import admin
 from shifts import views
 
 urlpatterns = [
+    url(r'^login/$', login, name='login'),
     url(r'^admin/', admin.site.urls),
-    url(r'^today/', views.onduty_today),
+    url(r'^today/', views.onduty_current_week),
+    url(r'^current_week/', RedirectView.as_view(url='/today')),
     url(r'^next_week/', views.onduty_next_week),
     url(r'^reset/', views.reset),
+    url(r'^shift_change/', views.shift_change),
+    url(r'^$', RedirectView.as_view(url='/today')),
 ]
