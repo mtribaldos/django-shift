ce7314e501d03190885d608a7f84a5343fde8905
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2016-09-28 16:27:01 +0200

    Bootstrap'ed login and shift change forms

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
+    'bootstrap_datepicker',
     'shifts'
 ]
 
@@ -120,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = "static"
 
 # Shift app setup
 import datetime
--- shifts/forms.py
+++ shifts/forms.py
@@ -1,11 +1,24 @@
 
 from django import forms
 import datetime
-from functools import partial
+#from functools import partial
 
-DateInput = partial(forms.DateInput, {'class': 'datepicker'})
+#DateInput = partial(forms.DateInput, {'class': 'datepicker'})
+
+from bootstrap_datepicker.widgets import DatePicker
 
 class ShiftChangeForm(forms.Form):
-    first_date = forms.DateField(label='Primer turno', widget=DateInput())
-    second_date = forms.DateField(label='Segundo turno', widget=DateInput())
+    options = { "format": "dd/mm/yyyy",
+                "autoclose": True,
+                "weekStart": 1,
+                "calendarWeeks": True,
+                "daysOfWeekDisabled": [1,2,3,4,5],
+                "startDate": '01/01/2016',
+                "todayHighlight": True,
+                "language": "es" }
+
+    first_date = forms.DateField(
+          widget=DatePicker(options = options))
+    second_date = forms.DateField(
+          widget=DatePicker(options = options))
 
