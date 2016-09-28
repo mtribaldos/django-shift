29ee336a63fe022363987db80f20dac1d9378b64
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2016-09-28 16:31:07 +0200

    Cleaned dead code in shift change form file

--- shifts/forms.py
+++ shifts/forms.py
@@ -1,24 +1,22 @@
 
 from django import forms
 import datetime
-#from functools import partial
-
-#DateInput = partial(forms.DateInput, {'class': 'datepicker'})
-
 from bootstrap_datepicker.widgets import DatePicker
 
+
 class ShiftChangeForm(forms.Form):
-    options = { "format": "dd/mm/yyyy",
-                "autoclose": True,
-                "weekStart": 1,
-                "calendarWeeks": True,
-                "daysOfWeekDisabled": [1,2,3,4,5],
-                "startDate": '01/01/2016',
-                "todayHighlight": True,
-                "language": "es" }
+    options = { 
+        "format": "dd/mm/yyyy",
+        "autoclose": True,
+        "weekStart": 1,
+        "calendarWeeks": True,
+        "daysOfWeekDisabled": [1,2,3,4,5],
+        "startDate": '01/01/2016',
+        "todayHighlight": True,
+        "language": "es" }
 
     first_date = forms.DateField(
-          widget=DatePicker(options = options))
+        widget=DatePicker(options = options))
     second_date = forms.DateField(
-          widget=DatePicker(options = options))
+        widget=DatePicker(options = options))
 
