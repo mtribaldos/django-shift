548e3fbe054feb0d4e01897a8ca5d28592369812
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2016-09-29 15:54:39 +0200

    Try to fix static assets for production

--- config/settings/base.py
+++ config/settings/base.py
@@ -182,9 +182,9 @@ STATIC_ROOT = normpath(join(SITE_ROOT, 'static'))
 STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = (
-    normpath(join(SITE_ROOT, 'static')),
-)
+#STATICFILES_DIRS = (
+#    normpath(join(SITE_ROOT, 'static')),
+#)
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = (
