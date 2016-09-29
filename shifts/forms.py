ffdbe2727ad65da2a78573726be3cd4a8e917552
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2016-09-29 23:08:18 +0200

    Reverted shift change form date format to dd/mm/yyyy

--- shifts/forms.py
+++ shifts/forms.py
@@ -6,7 +6,7 @@ from bootstrap_datepicker.widgets import DatePicker
 
 class ShiftChangeForm(forms.Form):
     options = { 
-        "format": "mm/dd/yyyy",
+        "format": "dd/mm/yyyy",
         "autoclose": True,
         "weekStart": 1,
         "calendarWeeks": True,
