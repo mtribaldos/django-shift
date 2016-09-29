b55c9d4f9e0e90cb4df8122beab3c32cc4aec633
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2016-09-29 15:25:00 +0200

    Fixed production settings file to use a full dictionary for _DATABASES_ variable

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,11 @@ from .base import *
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
