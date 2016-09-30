2e8369321031541b16bf3cf4847ab32d9b1e5358
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2016-09-30 15:48:32 +0200

    Refactored app urls layout

--- config/urls.py
+++ config/urls.py
@@ -1,20 +1,7 @@
 """django_shifts URL Configuration
 """
-from django.contrib.auth.views import login, logout
-from django.conf.urls import url
-from django.views.generic import RedirectView
-from django.contrib import admin
-from shifts import views
+from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^login/$', login, name='login'),
-    url(r'^logout/$', logout, name='logout'),
-    url(r'^change_password', views.change_password),
-    url(r'^admin/', admin.site.urls),
-    url(r'^current_week/', views.onduty_current_week),
-    url(r'^today/', RedirectView.as_view(url='/current_week')),
-    url(r'^next_week/', views.onduty_next_week),
-    url(r'^reset/', views.reset),
-    url(r'^switch_shifts/', views.shift_change),
-    url(r'^$', views.landing_page),
+    url(r'^', include('shifts.urls', namespace="shifts")),
 ]
