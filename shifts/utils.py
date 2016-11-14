d23e9cd65b9c560c9ec94bce9b5d9609ec68e9be
Miguel Ángel Tribaldos <mtribaldos@gmail.com>
2016-11-14 16:44:14 +0100

    Fix again first_date_of_year_week

--- shifts/utils.py
+++ shifts/utils.py
@@ -13,4 +13,4 @@ def current_year_week():
     return year_week(today)
 
 def first_date_of_year_week(week):
-    return settings.ANCHOR_DATE + datetime.timedelta(days=week*7) + datetime.timedelta(days=-(ANCHOR_DATE.isocalendar()[2] - 1))
+    return settings.ANCHOR_DATE + datetime.timedelta(days=week*7) + datetime.timedelta(days=-(settings.ANCHOR_DATE.isocalendar()[2] - 1))
